3ecc27728b1c5ec5b041cebf56bf7ee0bb4bb2f7
Understudystudent <carlmjames2@gmail.com>
2023-11-09 09:47:09 +0200

    changed the data-subit to data-addition

--- java/code.js
+++ java/code.js
@@ -1,8 +1,16 @@
 
 
+const btnAdd = document.querySelector('[data-addition]');
+const lblOutput = document.querySelector ('#output');
 
+function add(){
 
+let number1 =  parseInt (document.getElementById("number1").value);
+let number2 =  parseInt (document.getElementById("number2").value);
 
+let sum = number1 + number2;
 
+lblOutput.textContent =sum
+}   
 
-
+btnAdd.addEventListener('click',add)
