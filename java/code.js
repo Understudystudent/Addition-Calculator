3b2d59145189963a87b2d861c5229186badfb25c
Understudystudent <carlmjames2@gmail.com>
2023-11-09 10:54:24 +0200

    Used eval instead

--- java/code.js
+++ java/code.js
@@ -5,12 +5,14 @@ const lblOutput = document.querySelector ('#output');
 
 function add(){
 
-let number1 =  parseInt (document.getElementById("number1").value);
-let number2 =  parseInt (document.getElementById("number2").value);
+let number1 =  document.getElementById("number1").value;
+let number2 =  document.getElementById("number2").value;
 
-let sum = number1 + number2;
+let sum = eval(`${number1} + ${number2}`);
 
 lblOutput.textContent =sum
 }   
 
 btnAdd.addEventListener('click',add)
+
+// console.log(window);
